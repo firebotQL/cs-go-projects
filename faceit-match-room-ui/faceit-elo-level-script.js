c0af5b8313fe619a7de0c3c6a6a8c3df40c4e3ee
Viaceslavas Duk <viadukm@gmail.com>
2016-02-09 13:33:38 +0000

    Update faceit-elo-level-script.js

--- faceit-match-room-ui/faceit-elo-level-script.js
+++ faceit-match-room-ui/faceit-elo-level-script.js
@@ -7,6 +7,7 @@
 // @match        https://beta.faceit.com/en/csgo/room*
 // @grant        none
 // ==/UserScript==
+// CREDIT AND KUDOS TO: PyroZeroX. AngularJS example from: https://gist.github.com/PyroZeroX/9e75b2a205e842b0ecbb
 /* jshint -W097 */
 'use strict';
 
@@ -14,55 +15,90 @@
 var webkey = "YOUR STEAM WEB API KEY GOES HERE";
 
 angular.element(document).ready(function() {
-    // NOTE: if anyone knows how to plug in into angularjs controller to $watch when "members" has changed/loaded then
-    // we can get rid of setTimeout and all html scraping boilerplate(and maybe 10 get calls?) please let know. Thanks!
-    setTimeout(function() { 
-        var matchItems = $(".match-team-member");
-        $(".match-vs .match-team-member__row .match-team-member__name").each(function(idx, el) { 
-            var href = $(el).attr("href");
-            if (href) {
-                var splitHref = href.split("/");
-                var nickIdx = splitHref.length-1;
-                if (splitHref[nickIdx]) {
-                    $.get("https://api.faceit.com/api/nicknames/" + splitHref[nickIdx], function( data ) {
-                        //debugger;
-                        var csgoPayload = data['payload']['games']['csgo'];
-                        var steamID = csgoPayload['game_id'];
-                        var steamURL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + webkey + "&steamid=" + steamID + "&format=json"
+	angular.element(document).injector().invoke(function($compile) {
+        var gameElement = $("section.match-vs");
+        var game = angular.element(gameElement).scope();
 
-                        // Proxy based on https://github.com/afeld/jsonp 
-                        $.ajax({   url: "https://jsonp-steamid-proxy.herokuapp.com/?url=" + encodeURIComponent(steamURL),
-                                    success: function(data) { draw(data, csgoPayload, matchItems, idx); },
-                                    error: function(data) { draw(data, csgoPayload, matchItems, idx); }
-                        });
+        game.$watch('match',
+            function(match) {
+                if (match != null) {
+                    var factions = match.faction1.slice(0).concat(match.faction2);
+                    
+                    $(factions).each(function(index, player) {
+	                    var steamID = player['csgo_id'];
+	                    var steamURL = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + webkey + "&steamid=" + steamID + "&format=json"
+
+	                    // Proxy based on https://github.com/afeld/jsonp 
+	                    $.ajax({   url: "https://jsonp-steamid-proxy.herokuapp.com/?url=" + encodeURIComponent(steamURL),
+	                                success: function(data) { callfaceit(data, player, index); },
+	                                error: function(data) { callfaceit(data, player, index); }
+	                    });
                     });
                 }
             }
-        });
-    }, 3000);
+        );
+    });
 });
 
-var draw = function(data, csgoPayload, matchItems, idx) {
-      debugger;
-      var hours = "N/A";
-      try {
-        var games = data.response.games;
-        for(var i = 0; i < games.length; i++) {
-          var game = games[i];
-          if (game.appid === 730) {
-            hours = (game.playtime_forever/60).toFixed(1);
-          }
-        }
-      } catch (err) {
-    }
+var callfaceit = function(previousData, player, index) {
+	$.ajax({   url: "https://api.faceit.com/api/nicknames/" + player['nickname'], 
+				success: function(data) { 
+					try {
+						player = data['payload']['games']['csgo'];
+						player['hours'] = extractHours(previousData);
+					} catch (err) {}
+					drawcustomstats(data, player, index); 
+				},
+	            error: function(data) { 
+	            	player['hours'] = extractHours(previousData);
+	            	drawcustomstats(data, player, index); 
+	            }
+	});
+};
 
-    var style = "position: absolute; z-index: 3;right: 34px;bottom: -6px;width: 24px;height: 24px";
-    var imgHtml = "<div style='" + style + "'><img src='https://cdn.faceit.com/frontend/75/assets/images/skill-icons/skill_level_" + csgoPayload['csgo_skill_level'] + "_sm.png'/></div>";
-    var resultHTML = "<div class='pa-sm btn-block' style='padding-top: 0px !important;'>";
-    resultHTML += "<div > ELO: <strong>" + csgoPayload['faceit_csgo_elo'] + "</strong>" +
-    " Hours: <strong>" + hours + "</strong></div>";
-    resultHTML += "</div>";
+var extractHours = function(data) {
+	var hours = "N/A";
+	try {
+		var games = data['response']['games'];
+		for(var i = 0; i < games.length; i++) {
+		  var game = games[i];
+		  if (game['appid'] === 730) {
+		    hours = (game['playtime_forever']/60).toFixed(1);
+		    break;
+		  }
+		}
+	} catch (err) {}
+	return hours;
+};
 
-    $(matchItems[idx]).find('.match-team-member__avatar').append(imgHtml);
-    $(matchItems[idx]).find('.match-team-member__row').append(resultHTML);
+var drawcustomstats = function(data, player, index) {
+	var matchItem = $(".match-team-member:eq(" + index + ")");
+	
+    var skillLevelImgURL = "https://cdn.faceit.com/frontend/75/assets/images/skill-icons/skill_level_" + player['csgo_skill_level'] + "_sm.png";
+    var skillLevelDiv = $("<div></div>").addClass("custom_skill_level");
+    var skillLevelImg = $("<img/>").attr({ src: skillLevelImgURL})
+    skillLevelDiv.css({
+    			"position": "absolute",
+    			"z-index": 3,
+    			"right": "34px",
+    			"bottom": "-6px",
+    			"width": "24px",
+    			"height": "24px",
+	}).append(skillLevelImg);
+    
+	var statsContainerDiv = $("<div></div>").addClass('pa-sm')
+											.addClass('btn-block')
+											.addClass("custom_stats_field")
+											.attr('style', 'padding-top: 0px !important');
+	var statsDiv = $("<div></div>");
+	var eloField = $("<strong></strong>").html(player['faceit_csgo_elo']);
+	statsDiv.append("ELO: ");
+	statsDiv.append(eloField);
+	var hoursField = $("<strong></strong>").html(player['hours']);
+	statsDiv.append(" Hours: ")
+	statsDiv.append(hoursField);
+	statsContainerDiv.append(statsDiv);
+
+    matchItem.find('.match-team-member__avatar').append(skillLevelDiv);
+    matchItem.find('.match-team-member__row').append(statsContainerDiv);
 };
